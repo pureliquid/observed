2309fbf6b71f6cfefd7f11cb95cc67c50f10a049
PureLiquid6 <iampureliquid@pm.me>
2022-09-15 01:41:34 +0200

    adds HTMLElement watcher example

---
+++ src/examples/web/src/index.ts
@@ -0,0 +1,30 @@
+import { WatchThis } from '../../../observed/handler';
+import './index.html';
+
+const getObservedElement = <T extends HTMLElement>(elementOrSelector: T | null | string): T => {
+  // TODO: CACHE INSTANCES!!
+  let element: T | null = elementOrSelector as T;
+  if (typeof elementOrSelector === 'string') {
+    element = document.querySelector(elementOrSelector) as T;
+  }
+  if (!element) {
+    throw new Error('Missing DOM Element!!');
+  }
+  return WatchThis(
+    element,
+    function (changes: keyof T[]) {
+      console.log('changes ', changes);
+    },
+    function (read: keyof T[]) {
+      console.log('read ', read);
+    }
+  ) as T;
+};
+
+window.onload = () => {
+  const one = document.getElementById('one');
+  const cls = getObservedElement('#one');
+  console.log(cls.id);
+  console.log('OK: ', cls.getAttribute('id'));
+  cls.setAttribute('test', '1337');
+};
